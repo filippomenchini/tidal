92a320a4dd3b91bd5fb8ff24c30999869650707e
Filippo Menchini <33838929+filippomenchini@users.noreply.github.com>
2023-12-07 17:05:03 +0100

    Added CountryCode class

---
+++ lib/src/types/country_code.dart
@@ -0,0 +1,7 @@
+//ignore_for_file: non_constant_identifier_names
+
+class CountryCode {
+  static String get Usa => "US";
+  static String get Italy => "IT";
+  static String get defaultCountryCode => Usa;
+}
